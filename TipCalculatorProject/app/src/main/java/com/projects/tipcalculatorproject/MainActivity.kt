a4339da69a1731f78e02859ebdbd47470ee76bb1
Katie Borema <kborema@wccnet.edu>
2022-02-17 18:44:20 -0500

    update

--- TipCalculatorProject/app/src/main/java/com/projects/tipcalculatorproject/MainActivity.kt
+++ TipCalculatorProject/app/src/main/java/com/projects/tipcalculatorproject/MainActivity.kt
@@ -15,29 +15,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.tipBtn.setOnClickListener{
-            val billAmtEntered = binding.billAmount.text
-            var outputString = ""
-
-            if (billAmtEntered.isNotEmpty()) {
-                outputString = calculateTip(billAmtEntered.toString().toFloat())
-            } else {
-                outputString = "You must enter a bill amount!"
-            }
-            binding.output.text = outputString
+            binding.output.text = calculateTip(binding.billAmount.text.toString())
         }
     }
 
-    private fun calculateTip(billAmt: Float): String {
-        val tenPercentTotal = String.format("%.2f", billAmt * 1.10)
-        val fifteenPercentTotal = String.format("%.2f", billAmt * 1.15)
-        val twentyPercentTotal = String.format("%.2f", billAmt * 1.2)
+    private fun calculateTip(billAmt: String): String {
+        return if(billAmt.isEmpty()) {
+            "You must enter a bill amount!"
+        } else {
+            val billAmtToFloat = billAmt.toFloat()
+            val tenPercentTotal = String.format("%.2f", billAmtToFloat * 1.10)
+            val fifteenPercentTotal = String.format("%.2f", billAmtToFloat * 1.15)
+            val twentyPercentTotal = String.format("%.2f", billAmtToFloat * 1.2)
 
-        return "The tips are as follows: \n\n " +
-                "10% = $tenPercentTotal \n" +
-                "15% = $fifteenPercentTotal \n" +
-                "20% = $twentyPercentTotal"
-
-
+            "The tips are as follows: \n\n" +
+                    "10% = $tenPercentTotal \n" +
+                    "15% = $fifteenPercentTotal \n" +
+                    "20% = $twentyPercentTotal"
+        }
     }
 
 
