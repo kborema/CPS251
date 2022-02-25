7c6886041ce1a626c4a642faf9ed074ac396d585
Katie Borema <kborema@wccnet.edu>
2022-02-24 21:15:56 -0500

    Adding name save data 1 project

---
+++ NameSaveDataProject1/app/src/main/java/com/projects/namesavedataproject1/ui/main/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.projects.namesavedataproject1.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private var nameText = ""
+    private var namesList = arrayListOf<String>()
+
+    fun addName(name: String) {
+        this.nameText = name
+        this.namesList.add(name)
+    }
+
+    fun getNamesList(): String {
+        var listOfNames = ""
+
+        for (name in namesList) {
+            listOfNames += name + "\n"
+        }
+
+        return listOfNames
+    }
+
+}
