3c5e9637be16fe7303bca8e7c563274b8452a5ba
Katie Borema <kborema@wccnet.edu>
2022-04-09 11:41:20 -0400

    update

--- RecycleViewWithIntentsProject/app/src/main/java/com/projects/recycleviewprojectwithintents/MainActivity2.kt
+++ RecycleViewWithIntentsProject/app/src/main/java/com/projects/recycleviewprojectwithintents/MainActivity2.kt
@@ -2,10 +2,25 @@ package com.projects.recycleviewprojectwithintents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+
+import com.projects.recycleviewprojectwithintents.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val extras = intent.extras ?: return
+
+        val cardPosition = extras.getInt("position")
+        binding.titleTextView.text = Data.titles[Data.randomTitles[cardPosition]]
+        binding.detailTextView.text = Data.details[Data.randomDetails[cardPosition]]
+        binding.imageView.setImageResource(Data.images[Data.randomImages[cardPosition]])
     }
 }
